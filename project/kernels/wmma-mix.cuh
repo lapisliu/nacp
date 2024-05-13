73062fc240a369d63a2954b39c09535bd6bce4ca
Richard Xu <wx2006@nyu.edu>
2024-05-12 22:39:31 -0400

    try official wmma

--- project/kernels/wmma-mix.cuh
+++ project/kernels/wmma-mix.cuh
@@ -6,26 +6,70 @@ using namespace nvcuda;
 constexpr int WARP_SIZE = 16;
 
 __global__ void matMulWMMA(half *a, half *b, float *c, int M, int N, int K) {
-    int blockRow = blockIdx.y * blockDim.y + threadIdx.y;
-    int blockCol = blockIdx.x * blockIdx.x + threadIdx.x;
+    // int blockRow = blockIdx.y * blockDim.y + threadIdx.y;
+    // int blockCol = blockIdx.x * blockIdx.x + threadIdx.x;
 
+    // // Declare the fragments
+    // wmma::fragment<wmma::matrix_a, WARP_SIZE, WARP_SIZE, WARP_SIZE, half, wmma::col_major> a_frag;
+    // wmma::fragment<wmma::matrix_b, WARP_SIZE, WARP_SIZE, WARP_SIZE, half, wmma::row_major> b_frag;
+    // wmma::fragment<wmma::accumulator, WARP_SIZE, WARP_SIZE, WARP_SIZE, float> c_frag;
+
+    // // Initialize the output fragment
+    // wmma::fill_fragment(c_frag, 0.0f);
+
+    // int row = threadIdx.y;
+    // int col = threadIdx.x;
+    // for (int k = 0; k < K / WARP_SIZE; k++) {
+    //     if (blockRow < M && k * WARP_SIZE + col < K && blockCol < N && k * WARP_SIZE + row < K) {
+    //         wmma::load_matrix_sync(a_frag, a, WARP_SIZE);
+    //         wmma::load_matrix_sync(b_frag, b, WARP_SIZE);
+    //         wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+    //         wmma::store_matrix_sync(c, c_frag, WARP_SIZE, wmma::mem_row_major);
+    //     }
+    // }
+    // Leading dimensions. Packed with no transpositions.
+    int lda = M;
+    int ldb = K;
+    int ldc = M;
+    
+    // Tile using a 2D grid
+    int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / warpSize;
+    int warpN = (blockIdx.y * blockDim.y + threadIdx.y);
     // Declare the fragments
-    wmma::fragment<wmma::matrix_a, WARP_SIZE, WARP_SIZE, WARP_SIZE, half, wmma::col_major> a_frag;
-    wmma::fragment<wmma::matrix_b, WARP_SIZE, WARP_SIZE, WARP_SIZE, half, wmma::row_major> b_frag;
-    wmma::fragment<wmma::accumulator, WARP_SIZE, WARP_SIZE, WARP_SIZE, float> c_frag;
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> a_frag;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> acc_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> c_frag;
+    wmma::fill_fragment(acc_frag, 0.0f);
+    // Loop over the K-dimension
+    for (int i = 0; i < K; i += WMMA_K) {
+        int aRow = warpM * WMMA_M;
+        int aCol = i;
+        int bRow = i;
+        int bCol = warpN * WMMA_N;
+        
+        // Bounds checking
+        if (aRow < M && aCol < K && bRow < K && bCol < N) {
+            // Load the inputs
+            wmma::load_matrix_sync(a_frag, a + aRow + aCol * lda, lda);
+            wmma::load_matrix_sync(b_frag, b + bRow + bCol * ldb, ldb);
 
-    // Initialize the output fragment
-    wmma::fill_fragment(c_frag, 0.0f);
-
-    int row = threadIdx.y;
-    int col = threadIdx.x;
-    for (int k = 0; k < K / WARP_SIZE; k++) {
-        if (blockRow < M && k * WARP_SIZE + col < K && blockCol < N && k * WARP_SIZE + row < K) {
-            wmma::load_matrix_sync(a_frag, a, WARP_SIZE);
-            wmma::load_matrix_sync(b_frag, b, WARP_SIZE);
-            wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
-            wmma::store_matrix_sync(c, c_frag, WARP_SIZE, wmma::mem_row_major);
+            // Perform the matrix multiplication
+            wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
         }
+    }
+    // Load in current value of c, scale by beta, and add to result scaled by alpha
+    int cRow = warpM * WMMA_M;
+    int cCol = warpN * WMMA_N;
+    
+    if (cRow < M && cCol < N) {
+        wmma::load_matrix_sync(c_frag, c + cRow + cCol * ldc, ldc, wmma::mem_col_major);
+        
+        for(int i=0; i < c_frag.num_elements; i++) {
+            c_frag.x[i] = alpha * acc_frag.x[i] + beta * c_frag.x[i];
+        }
+        // Store the output
+        wmma::store_matrix_sync(c + cRow + cCol * ldc, c_frag, ldc, wmma::mem_col_major);
     }
 }
 
